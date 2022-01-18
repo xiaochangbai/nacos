ffecdabe7dc946aa5bc329c84ce95469788e080f
xiaochangbai <704566072@qq.com>
2022-01-18 10:19:29 +0800

    feat: 设置单机启动

--- console/src/main/java/com/alibaba/nacos/Nacos.java
+++ console/src/main/java/com/alibaba/nacos/Nacos.java
@@ -16,6 +16,7 @@
 
 package com.alibaba.nacos;
 
+import com.alibaba.nacos.sys.env.Constants;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -32,6 +33,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class Nacos {
     
     public static void main(String[] args) {
+        System.setProperty(Constants.STANDALONE_MODE_PROPERTY_NAME,"true");
         SpringApplication.run(Nacos.class, args);
     }
 }
